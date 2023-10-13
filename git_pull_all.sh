c3cb282f2b819b102893a4af073202d19371feee
DenysD <ddemiankov@adva.com>
2023-10-13 16:38:16 +0200

    Refactor git_pull_all.sh: Replace array with temp file for error tracking and simplify directory change logic.

--- git_pull_all.sh
+++ git_pull_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# An array to hold repositories where errors occurred
-declare -a failed_repos=()
+# Temporary file to hold repositories where errors occurred
+failed_repos_file=$(mktemp)
 
 # Functions to print messages
 print_error() {
-    printf '\033[0;31mError: %s\033[0m\n' "$1"
+    printf '\n\033[0;31mError: %s\033[0m\n' "$1"
 }
 export -f print_error
 
@@ -20,23 +20,21 @@ perform_git_operations() {
     local original_dir=$(pwd)
     local repo_output=""  # to hold output for this repository
 
-    # Ensure we always return to the original directory
-    trap 'cd "$original_dir"' RETURN
-
-    cd "$repo_dir" || { repo_output+=$(print_error "Cannot change to directory: $repo_dir"); failed_repos+=("$repo_dir"); return 1; }
+    cd "$repo_dir" || { repo_output+=$(print_error "Cannot change to directory: $repo_dir"); echo "$repo_dir" >> "$2"; return 1; }
 
     repo_output+=$(print_info "Processing repository in directory: $repo_dir")
 
-    # git pull &>/dev/null
     git pull
     if [ $? -ne 0 ]; then
         repo_output+=$(print_error "Cannot pull latest changes in repository: $repo_dir")
-        failed_repos+=("$repo_dir")
+        echo "$repo_dir" >> "$2"
         printf "%b" "$repo_output\n"
+        cd "$original_dir"
         return 1
     fi
 
     printf "%b" "$repo_output\n"
+    cd "$original_dir"
 }
 
 # Export function so it's available in the subshell used by xargs
@@ -46,17 +44,18 @@ export -f perform_git_operations
 print_info "Script started..."
 
 # Find git repositories and process them concurrently
-find . -type d -name ".git" -print0 | xargs -0 -n1 -I{} -P8 bash -c 'dir=$(dirname "$1"); perform_git_operations "$dir"' _ {}
+find . -type d -name ".git" -print0 | xargs -0 -n1 -I{} -P8 bash -c 'dir=$(dirname "$1"); perform_git_operations "$dir" "'"$failed_repos_file"'"' _ {}
 
 # Print ending message
 print_info "Script finished."
 
 # Print repositories where errors occurred
-if [ ${#failed_repos[@]} -eq 0 ]; then
+if [ ! -s "$failed_repos_file" ]; then
     print_info "No errors occurred in any repositories."
 else
     print_error "Errors occurred in the following repositories:"
-    for repo in "${failed_repos[@]}"; do
-        echo "$repo"
-    done
+    cat "$failed_repos_file"
 fi
+
+# Clean up temporary file
+rm -f "$failed_repos_file"
